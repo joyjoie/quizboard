a44b2c94da86d025144512340de8d8f2b8a757e6
joy <wendokingoo@gmail.com>
2018-12-08 19:27:12 +0300

    Changes updated in the index file and javascript elememnts added

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var allQuestions =[{ QUESTION:".que1",choices:["HTML","CSS","JS"],correctAnswer:1};
+  {QUESTION:".que2",choices:["#gitpush","#gitcommit","#gitremoteadd"],correctAnswer:2};
+  {QUESTION:".que3",choices:["#else","#for","#var"],correctAnswer:1};
+  {QUESTION:".que4",choices:["#yr1","#yr2","#yr3"],correctAnswer:2};
+  {QUESTION:".que5",choices:["#yep","#nay"],correctAnswer:2}];
