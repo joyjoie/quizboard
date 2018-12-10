d1cf1efc6be5a1c385966ffea297a799229e80ae
joy <wendokingoo@gmail.com>
2018-12-10 10:12:30 +0300

    spanid element added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 
-  $(document).ready(function(){
+  $(document).ready(function){
       $(".form-group2,.form-group3,.form-group4,.form-group5").hide();
 
       var answer1 = $("input:radio[name=question1]").val();
         $("#fnext").click(function(){
       if(answer1 == "CSS"){
         $(".form-group2").show();
+        $('#spanid').html('&#10004;');
         console.log("Good Work!");
       }
       else{
@@ -18,6 +19,7 @@
       var answer2 =$("input:radio[name=question2]").val();
         if(answer2 == "Git remote add"){
           $(".form-group3").show();
+          $('#spanid').html('&#10004;');
           console.log("You're doing good proceed");
         }
         else{
@@ -30,6 +32,7 @@
         var answer3 =("input:[name=question3]").val();
         if(answer3 == "For"){
           $(".form-group3").show();
+          $('#spanid').html('&#10004;');
           console.log("You're doing good proceed");
         }
         else{
@@ -42,6 +45,7 @@
         var answer4 =("input:[name=question4]").val();
         if(answer4 == "2020"){
           $(".form-group4").show();
+          $('#spanid').html('&#10004;');
           console.log("You're doing good proceed");
         }
         else{
@@ -54,6 +58,7 @@
         var answer5 =("input:[name=question5]").val();
         if(answer5 == "Yes"){
           $(".form-group3").show();
+          $('#spanid').html('&#10004;');
           console.log("You're done with all your questions");
           var total=
         }
