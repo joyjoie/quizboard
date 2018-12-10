6ea8c0ba7bd5b20ec759047304f6fd00cfc55e02
joy <wendokingoo@gmail.com>
2018-12-10 09:48:56 +0300

    if else elements added for each question

--- js/script.js
+++ js/script.js
@@ -1,47 +1,47 @@
-// var allQuestions =$('.form-group').size();
-// var allQuestions =[{ QUESTION:".que1",choices:["HTML","CSS","JS"],correctAnswer:1};
-//   {QUESTION:".que2",choices:["#gitpush","#gitcommit","#gitremoteadd"],correctAnswer:2};
-//   {QUESTION:".que3",choices:["#else","#for","#var"],correctAnswer:1};
-//   {QUESTION:".que4",choices:["#yr1","#yr2","#yr3"],correctAnswer:2};
-//   {QUESTION:".que5",choices:["#yep","#nay"],correctAnswer:2}];
-//   var currentQuestion =0;
-//   $questions =$(".form-group");
-//   $questions.hide();
-//   $(questions.get(currentQuestion)).fadeIn();
-//   $("#fnext").click(function(){
-//
-//     $($questions.get(currentQuestion)).fadeOut(function(){
-//       currentQuestion=currentQuestion +1;
-//     }
-//   }
-//   function fnext{
-//
-//   }
-//   function snext{
-//
-//   }
-//   function tnext{
-//
-//   }
-//   function rnext{
-//
-//   }
-//   function submit{
-//
-//   }
 
   $(document).ready(function(){
       $(".form-group2,.form-group3,.form-group4,.form-group5").hide();
 
-      var answer1 = $("input:radio[name=question1];").val();
+      var answer1 = $("input:radio[name=question1]").val();
 
       if(answer1 == "CSS"){
         $(".form-group2").show();
-        console.log("Good Work!")
+        console.log("Good Work!");
       }
       else{
         console.log("You have answered incorrectly.Try again")
-      }
-
+      };
+      var answer2 =$("input:radio[name=question2]").val();
+        if(answer2 == "Git remote add"){
+          $(".form-group3").show();
+          console.log("You're doing good proceed");
+        }
+        else{
+          console.log("Error.Try again")
+        };
+        var answer3 =("input:[name=question3]").val();
+        if(answer3 == "For"){
+          $(".form-group3").show();
+          console.log("You're doing good proceed");
+        }
+        else{
+          console.log("Error.Try again")
+        };
+        var answer4 =("input:[name=question4]").val();
+        if(answer4 == "2020"){
+          $(".form-group4").show();
+          console.log("You're doing good proceed");
+        }
+        else{
+          console.log("Error.Try again")
+        };
+        var answer5 =("input:[name=question5]").val();
+        if(answer5 == "Yes"){
+          $(".form-group3").show();
+          console.log("You're doing good proceed");
+        }
+        else{
+          console.log("Error.Try again")
+        };
 
   });
